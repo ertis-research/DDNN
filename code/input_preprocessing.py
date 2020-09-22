acd4b495a376e9b93fabad3f3df2fdb362a273aa
Daniel R. Torres Ruiz <danielr.torresruiz@gmail.com>
2020-09-22 17:28:27 +0200

    [MODIFY] `preprocess` function parameters
    This function now receives a list with the labels.

--- code/input_preprocessing.py
+++ code/input_preprocessing.py
@@ -1,8 +1,10 @@
 from PIL import Image
 
-def preprocess( list_images ):
+def preprocess( labels, list_images ):
     
     print( "Total images: {}.".format( len(list_images) ) )
     
-    Image.open( str(list_images[1]) ).show()
-    print( "Image {} shown.".format( list_images[1] ) )
+    print( list_images["daisy"][1] )
+    Image.open( str(list_images["daisy"][1]) ).show()
+    print( "Image {} shown.".format( list_images["daisy"][1] ) )
+    print( labels )
