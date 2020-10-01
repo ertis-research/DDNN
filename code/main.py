073d09694f057d76e75fec80fe1c63fec771ca9f
Daniel R. Torres Ruiz <danielr.torresruiz@gmail.com>
2020-10-01 14:08:32 +0200

    [MODIFY] Inference + Results
    Inference produced in one machine is completed. Additionally, the results of the evaluation are shown as specified in `output_results.py`.

--- code/main.py
+++ code/main.py
@@ -5,6 +5,7 @@ from output_results import output
 
 import argparse
 import pathlib
+import timeit
 
 # References:
 # https://www.tensorflow.org/tutorials/images/classification
@@ -27,8 +28,12 @@ def read_images( i_path, i_format, label_file_path ):
     
     return labels, images
 
-def error_input( p ):
-    p.error( "The --input (-i) argument requires the --labels arguments." )
+def error_input( p, error ):
+    error_message = {
+        0: "The --input (-i) argument requires the --labels arguments.",
+        1: "If no model is specified, you need to pass the direction of the device which will process the information."
+    }
+    p.error( error_message[ error ] )
 
 def main():
 
@@ -48,7 +53,7 @@ def main():
     parser.add_argument( '--cloud', help="This device computes entries as a cloud device.", nargs='?', const=True, default=False, type=bool )
     
     parser.add_argument( '-n', '--next-device', help="Indicates the value of the IP to send the inferance values obtained." )
-    parser.add_argument( '-t', '--threshold', help="Minimum value to be accepted as a correct value.", type=float)
+    parser.add_argument( '-t', '--threshold', help="Minimum value to be accepted as a correct value.", type=float, default=0.8)
     args = parser.parse_args()
 
     if args.tensorflow:
@@ -61,7 +66,7 @@ def main():
     if args.input:
 
         if not args.labels:
-            error_input( parser )
+            error_input( parser, 0 )
 
         else:
 
@@ -73,7 +78,12 @@ def main():
             if not args.models:
                 # If no model is passed, this code will only pass data after preprocessing them and
                 # recover the output of another model in a higher layer of the architecture.
-                print( args.next_device )
+                if not args.next_device:
+                    error_input( parser, 1 )
+                else:
+
+                    from kafka import KafkaConsumer, KafkaProducer
+                    print( args.next_device )
 
             else:
                 # If a model or models are passed as argument, it will inference using them in the order they are
@@ -84,16 +94,33 @@ def main():
                 for model_path in args.models:
                     models.append( tf.keras.models.load_model( model_path ) )
 
-                ## Inference
-                _y = []
-                for input_i in x:
+                if not args.edge and not args.fog and not args.cloud:
+                    ## Inference
+                    _y = []
+                    start_global_time = timeit.default_timer()
+                    times = []
+                    for input_i in x:
 
-                    _x = input_i
-                    for model in models:
-                        _x = model.predict( _x )
-                    _y.append(_x)
+                        start_prediction_time = timeit.default_timer()
+                        _x = input_i
+                        for model in models:
+                            _x = model.predict( _x )
+                            if len( _x ) > 1:
+                                if _x[-1:][0].max() >= args.threshold:
+                                    _x = _x[-1:][0][0]
+                                    break
+                        _y.append( _x )
+                        times.append( timeit.default_timer() - start_prediction_time )
+                else:
+                    # Here, models are loaded, but no data and so need to received and send it back.
+                    if args.edge:
+                        print( "EDGE" )
+                    elif args.fog:
+                        print( "FOG" )
+                    elif args.cloud:
+                        print( "CLOUD" )
 
-            output( y, _y )
+                output( y, _y, timeit.default_timer() - start_global_time, times )
 
 if __name__ == '__main__':
     main()
--- code/output_results.py
+++ code/output_results.py
@@ -1,2 +1,20 @@
-def output( expected_results, results ):
-    print( "Results: {}".format( results ) )
+from statistics import mean 
+
+def output( expected_results, results, global_time, prediction_times ):
+
+    accuracy = 0
+    loss = 0
+    for expected_result, result in zip( expected_results, results ):
+
+        result_i = result.argmax()
+        expected_result_i = expected_result.argmax()
+        accuracy += 1 if expected_result_i == result_i else 0
+        loss += ( expected_result[ expected_result_i  ] - result[ result_i ] ) ** 2
+
+    print( "\nTotal number of predictions: {}\nAccuracy: {}\nLoss: {}\nGlobal time: {}\nAverage Time per Prediction: {}".format( 
+        len( results ),
+        accuracy / len( results ), 
+        loss, 
+        global_time, 
+        mean( prediction_times ) 
+    ))
