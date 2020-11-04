13aad5907ef5fb990c8336ab164091c6dd627553
Daniel R. Torres Ruiz <danielr.torresruiz@gmail.com>
2020-11-04 09:53:40 +0100

    [MODIFY] `label_mode` is now included

--- code/model_training.py
+++ code/model_training.py
@@ -6,16 +6,16 @@ import argparse
 from model import create_model 
 from training_results import show_results
 
-def read_data( path, vsplit, seed, image_height, image_width, batch_size):
+def read_data( path, vsplit, seed, image_height, image_width, batch_size, label_mode):
     import pathlib
     from tensorflow import keras
-
-    data_dir = pathlib.Path( path )
+    
+    data_dir = pathlib.Path( path )    
     train_ds = keras.preprocessing.image_dataset_from_directory( data_dir, validation_split=vsplit, subset="training", seed=seed, 
-        image_size=( image_height, image_width ), batch_size=batch_size )
+        image_size=( image_height, image_width ), batch_size=batch_size, label_mode=label_mode )
 
     val_ds = keras.preprocessing.image_dataset_from_directory( data_dir, validation_split=vsplit, subset="validation", seed=seed,
-        image_size=( image_height, image_width ), batch_size=batch_size )
+        image_size=( image_height, image_width ), batch_size=batch_size, label_mode=label_mode )
 
     return train_ds, val_ds, train_ds.class_names
 
@@ -43,6 +43,7 @@ def main():
     parser.add_argument( '-w', '--image_width', type=int, help="Image width for training the model.", default=180 )
     parser.add_argument( '-s', '--seed', type=int, help="Seed used in random operations.", default=123)
     parser.add_argument( '-v', '--vsplit', type=float, help="Validation split.", default=0.2)
+    parser.add_argument( '-l', '--label_mode', help="int, categorical, binary or None. Default categorical", default="categorical")
     
     args = parser.parse_args()
 
@@ -51,7 +52,7 @@ def main():
         import tensorflow as tf
         print( "\tNOTE: Using Tensorflow {}.".format( tf.__version__ ) )
         
-        train_ds, val_ds, class_names = read_data( args.training_set, args.vsplit, args.seed, args.image_height, args.image_width, args.batch_size )
+        train_ds, val_ds, class_names = read_data( args.training_set, args.vsplit, args.seed, args.image_height, args.image_width, args.batch_size, args.label_mode )
 
         models = create_model( args.image_height, args.image_width, len( class_names ))
         # model.summary()
@@ -63,7 +64,7 @@ def main():
         )
 
         save_models( models, history, args.output )
-        show_results( history, args.epochs )
+        # show_results( history, args.epochs )
 
 if __name__ == '__main__': 
     main()
